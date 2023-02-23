1c48849c8775baf490687cc3224ba43362cfcc47
ryanott01 <108499594+ryanott01@users.noreply.github.com>
2023-02-22 20:15:46 -0700

    first commit

---
+++ forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Length
+
+
+class TeamForm(FlaskForm):
+    team_name = StringField(
+        'Team Name', validators=[DataRequired(), Length(min=4, max=255)]
+    )
+    submit = SubmitField("Submit")
+
+
+class ProjectForm(FlaskForm):
+    project_name = StringField(
+        'Project Name', validators=[DataRequired(), Length(min=4, max=255)]
+    )
+    description = TextAreaField('Description')
+    completed = BooleanField("Completed?")
+    team_selection = SelectField("Team")
+    submit = SubmitField("Submit")
+
+    def update_teams(self, teams):
+        self.team_selection.choices = [(team.id, team.team_name) for team in teams]
+
